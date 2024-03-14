be52066d7d05239868c8cc33583fd6b1374fea22
inikis <dinovelley@gmail.com>
2024-03-14 16:14:44 -0300

    campo de bits

--- float.c
+++ float.c
@@ -1,21 +1,48 @@
 #include <stdio.h>
 
 int main(void){
-    int numero;
+    float numero;
 
     printf("Elija su numero: ");
-    scanf("%d", &numero);
+    scanf("%f", &numero);
 
-    union decimal{
+    union{
         float var;
         unsigned char vect[4];
-    }numeroDecimal;
+        struct //campo de bits
+        {
+            unsigned int bit0 : 1;
+            unsigned int bit1 : 1;
+            unsigned int bit2 : 1;
+            unsigned int bit3 : 1;
+            unsigned int bit4 : 1;
+            unsigned int bit5 : 1;
+            unsigned int bit6 : 1;
+            unsigned int bit7 : 1;
+            unsigned int bit8 : 1;
+            unsigned int bit9 : 1;
+            unsigned int bit10 : 1;
+            unsigned int bit11 : 1;
+            unsigned int bit12 : 1;
+            unsigned int bit13 : 1;
+            unsigned int bit14 : 1;
+            unsigned int bit15 : 1;
+        };
+    }floatNum;
 
-    numeroDecimal.var=numero;
-
-    //printf("\nExpresar en... \n\t1. Hexa\n\t2. Binario\n\n");
+    floatNum.var=numero;
 
     for(int i=0; i<4; i++){
-        printf("%x |",numeroDecimal.vect[i]);
+        printf("%x |",floatNum.vect[i]);
     }
+
+    int bitElegido = 0;
+    int nuevoValor = 0;
+    printf("\nElija un bit del 0 al 15 para modificar de su float: ");
+    scanf("%d", &bitElegido);
+
+    printf("\nElija el nuevo valor (0 o 1) del bit %d: ",&bitElegido);
+    scanf("%d", &nuevoValor);
+
+    //TODO: como definir el bit especificado
 }
